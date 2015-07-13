b8d439f22bfbcfdf5d4ab7cc8883ab9918995503
johnhmacleod <john.h.macleod@gmail.com>
2015-07-13 11:28:47 +1000

    First Commit

---
+++ src/js/jscode.js
@@ -0,0 +1,234 @@
+var turl = localStorage.getItem('turl') ? localStorage.getItem('turl') : ''; 
+var tcode = localStorage.getItem('tcode') ? localStorage.getItem('tcode') : 'NSW_TP007';
+var tlevels = localStorage.getItem('tlevels') ? localStorage.getItem('tlevels') : '0';
+// console.log('tlevel from storage: ' + tlevels);
+var tdiff;
+var tpc;
+var highlow;
+var lastLevel;
+var nextLevel;
+var followingLevel;
+var followingi;
+
+var turl1 = "http://www.bom.gov.au/australia/tides/print.php?aac=";
+var turl2 = "&type=tide&tz=Australia/";
+var turl3 = "&days=2&date=";
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function sendResponse() {
+  console.log('sendreposne');
+      var dictionary = {        
+        "KEY_NEXT_TIDE" : tdiff,
+        "KEY_HIGHLOW" : highlow,
+        "KEY_PC" : tpc,
+        "KEY_LAST_LEVEL" : lastLevel,
+        "KEY_NEXT_LEVEL" : nextLevel,
+        "KEY_FLW_LEVEL" : followingLevel,
+        "KEY_CONFIG_LEVELS" : parseInt(tlevels,10)
+      };
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Tide info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending Tide info to Pebble!");
+        }
+      );
+}
+
+var nt = ""; //Next Tide
+var t1 = ""; //Tide info
+var range;
+
+function getTide() {
+  console.log("Get Tide");
+  var d = new Date();
+//  var url2 = url + d.getDate() + "-" + (d.getMonth()+1) + "-" + d.getFullYear(); // + url1;
+  var state = tcode.substring(0,3);
+  var city;
+    if (state=='NSW')
+      city = 'Sydney';
+    else if (state=='VIC')
+      city = 'Melbourne';
+    else if (state=='QLD')
+      city = 'Brisbane';
+    else if (state=='NT_')
+      city = 'Darwin';
+    else if (state=='TAS')
+      city = 'Hobart';
+    else if (state=='WA_')
+      city = 'Perth';
+    else if (state=='SA_')
+      city = 'Adelaide';
+      
+  var url2 = turl1 + tcode + turl2 + city + turl3 + d.getDate() + "-" + (d.getMonth()+1) + "-" + d.getFullYear();
+  console.log(url2);
+    xhrRequest(url2, 'GET', 
+    function(responseText) {
+
+      // Assemble dictionary using our keys
+      // var response = responseText;
+    HTMLTable(responseText);
+    sendResponse();
+    }      
+  );
+}
+var r = [];
+var s = [];
+var t = [];
+
+function HTMLTable(text) {
+  var i;
+  var d = new Date();
+  var addDay = 0;
+  var ampm, hrs, mins, diff;
+  
+  nt = "";
+  range = "";
+  t1 = "";
+  
+  // Need to look over 7 iterations for all today's tides (up to 5) and the first 2 tomorrow
+  for (i = 0; i < 7; i++) {
+    text = text.substring(text.search(/<th rowspan=[^=]*..instance[^>]*tide.>/));
+    r[i] = text.substring(text.search(">")+1,text.search(/<\/th>/));
+    text = text.substring(text.search(/<td[^=]*..localtime[^>]*>/));
+    s[i] = text.substring(text.search(">")+1,text.search(/<\/td>/)).replace(" ","");
+    text = text.substring(text.search(/<td[^=]*..height[^>]*>/));
+    t[i] = text.substring(text.search(">")+1,text.search(/<\/td>/));
+//    console.log(i +"->"+ "r[i]="+r[i]+ " s[i]="+s[i]+ " t[i]="+t[i]);
+    
+    hrs = Number(s[i].substring(0,s[i].search(":")));
+    mins = Number(s[i].substring(s[i].search(":")+1,s[i].search(/[ap]m/)));
+
+    var pmFlag = s[i].search("am") < 0; //True if PM
+//    console.log(i + "-> hrs: " + hrs + " mins: " + mins + " pmFlag: " + pmFlag);
+    if (!pmFlag && ampm == 12)  //We were previously correcting for PM but now back to AM - we've skipped to the next day
+      addDay = 24 * 3600 * 1000; // One day in msec
+    else addDay = 0;
+//    console.log(i + "-> addDay " + addDay);
+    
+    if (pmFlag && hrs < 12)  // Must be PM (but only need to adjust when hours go past 12)
+      ampm = 12;
+    else
+      ampm = 0;
+    
+    if (!pmFlag && hrs == 12) 
+      hrs = 0;
+//    console.log(i + "-> hrs (after midday correction): " + hrs + " ampm: " + ampm);
+//    console.log('Year: '+d.getFullYear()+' Month:'+d.getMonth()+' Date: '+d.getDate()+' hrs: '+(hrs+ampm)+' mins: '+mins);
+    var d2 = new Date(d.getFullYear(), d.getMonth(), d.getDate(),  hrs + ampm, mins, 0, 0); // Time of tide
+//    console.log('d: '+d+' d2: '+d2);
+    diff = addDay + d2.getTime() - d.getTime();
+//    console.log ("addDay: " + addDay + " Diff: " + diff);
+   if (diff > 0) {
+  // Later than now
+  
+    var hdiff = (diff / 3600000) | 0;
+    var mdiff = (((diff/3600000) - hdiff) * 60 ) | 0;
+//     console.log(i +"->"+ " Hdiff=" + hdiff + " hdiff /3600000=" +diff/3600000+ " Mdiff=" + mdiff);
+    if (nt === "") // We have not yet found the next tide
+      {
+        tpc = 0;
+        if (i > 0) // This is not the first tide of the day so we can work out the range
+          {
+            var lvl1, lvl2, pc;
+            lvl1 = Number(t[i-1].replace("m",""));  // Get rid of the metres symbole
+            lvl2 = Number(t[i].replace("m","")); // Get rid of the netres symbol
+            pc = Math.abs(lvl1-lvl2) / 1.89 * 100; // Adjust for max possible tide
+            range = "\n" + (pc | 0) + "% max range";
+ //           console.log("Range: " + range);
+            tpc = pc | 0;
+            lastLevel = t[i-1].replace(" ","");
+          }
+        else {
+          lastLevel = "n/a";
+          range = "";
+        }
+        nextLevel = t[i].replace(" ","");
+        followingi = i+1;
+     if (hdiff === 0) // Less than an hour to go
+       nt = r[i] + " in " + mdiff + "m";
+     else // More than an hour to go
+       nt = r[i] + " in " + hdiff + "h " + mdiff + "m";
+     nt = nt + range;
+        
+        highlow = r[i];
+        tdiff = hdiff*60 + mdiff; // In Minutes
+      }
+//    console.log(i +"->"+" Next " + r[i] + " tide in " + hdiff + " hours" + mdiff + " mins");
+//    console.log(i + "->" + d.getFullYear() + ":" + d.getMonth()+":"+d.getDate()+":"+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+":::" + d.getTime());
+//    console.log(i + "->" + d2.getFullYear() + ":" + d2.getMonth()+":"+d2.getDate()+":"+d2.getHours()+":"+d2.getMinutes()+":"+d2.getSeconds()+":::" + d2.getTime());
+   }
+
+    t1 = t1 + s[i] + " " + t[i].replace(" ","") + "\n"; // Get rid of needless spaces
+  }
+//  console.log("followingi: " + followingi);
+  followingLevel = t[followingi].replace(" ","");
+  if (tcode != "NSW_TP007") // Do the range % calculation for Fort Dennison
+    tpc = 0;
+}
+
+Pebble.addEventListener('showConfiguration', function(e) {
+//  console.log('showConfiguration: tcode:' + tcode);
+  var murl = 'http://johnpebble.ucoz.com/TideWatch.html?turl=' + encodeURIComponent(tcode) + '&tcode=' + encodeURIComponent(tcode) +
+      '&tlevels=' + encodeURIComponent(tlevels==1?'true':'false');
+//  console.log("Settings Url: " + murl);
+  Pebble.openURL(murl);
+});
+
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+    getTide();  // Delivers the tide reading to the waiting pebble!
+  }
+);
+
+Pebble.addEventListener('webviewclosed',
+  function(e) {
+//    console.log('Configuration window returned: ' + e.response);
+    var options = JSON.parse(decodeURIComponent(e.response));
+    var tmp = decodeURIComponent(options.turl);
+    if (tmp != 'undefined')
+      turl = tmp;
+//    console.log("Settings returned turl: " + tmp);
+    localStorage.setItem('turl', turl);    
+    tmp = decodeURIComponent(options.tcode);
+    if (tmp != 'undefined') 
+      tcode = tmp;
+//    console.log("Settings returned tcode: " + tmp);
+    localStorage.setItem('tcode', tcode);  
+    
+        tmp = decodeURIComponent(options.tlevels);
+    if (tmp != 'undefined') 
+      tlevels = tmp=='true'? 1:0;
+//    console.log("Settings returned tlevels: " + tmp);
+    localStorage.setItem('tlevels', tlevels);  
+    
+    getTide();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+//    console.log("[JAVASCRIPT] Received message from PEBBLE: " +
+//                JSON.stringify(e.payload));
+    
+    if (typeof e.payload["KEY_GETTIDE"] != "undefined")
+      {
+        getTide();
+      }
+  }                     
+);
+
---
+++ src/segment_six.c
@@ -0,0 +1,475 @@
+#include "pebble.h"
+#define KEY_NEXT_TIDE 1
+#define KEY_HIGHLOW 2
+#define KEY_GETTIDE 3
+#define KEY_READY 4
+#define KEY_PC 5
+#define KEY_LAST_LEVEL 6
+#define KEY_NEXT_LEVEL 7
+#define KEY_FLW_LEVEL 9
+#define KEY_CONFIG_LEVELS 10
+  
+static const char* tide_labels[] = {"5", "4", "3", "2", "1" };  
+static const GRect tide_pos1[] = {{{90,27},{18,18}}, {{109,43},{18,18}}, {{118,72},{18,18}}, {{110,97},{18,18}}, {{90,117},{18,18}}};
+static const GRect tide_pos2[] = {{{40,117},{18,18}}, {{18,98},{18,18}}, {{13,72},{18,18}}, {{19,43},{18,18}}, {{40,27},{18,18}}};
+static const GPathInfo MINUTE_SEGMENT_PATH_POINTS = {
+  3,
+  (GPoint[]) {
+    {0, 0},
+    {-6, -70}, // 80 = radius + fudge; 8 = 80*tan(6 degrees); 6 degrees per minute;
+    {6,  -70},
+  }
+};
+
+static const GPathInfo HOUR_SEGMENT_PATH_POINTS = {
+  3,
+  (GPoint[]) {
+    {0, 0},
+    {-20, -70}, // 50 = radius + fudge; _ = 50*tan(15 degrees); 30 degrees per hour;
+    {20,  -70},
+  }
+};
+#ifndef PBL_COLOR
+static GPath *s_hour_hand_path;
+static const GPathInfo HOUR_HAND_PATH_POINTS = {
+  6,
+  (GPoint[]) {
+    {-2,-55},
+      {0,-57},
+    {2,-55},
+    {4,0},
+    {0,2},
+    {-4,0},
+  }
+};
+
+#endif
+static void request_tide();
+static bool showLevels = false;
+static bool levelsAlways = false;
+static char highlow[10] = "";
+static int next_tide = -1;
+static int pc = 0;
+static char llvl[10], nlvl[10], flvl[10];
+static Window *s_main_window;
+static Layer *s_minute_display_layer, *s_hour_display_layer, *s_segment_layer;
+
+static GPath *s_minute_segment_path, *s_hour_segment_path;
+
+static void paint(GContext *ctx, GPath *path, int angle, GColor colour) {
+  gpath_rotate_to(path, (TRIG_MAX_ANGLE / 360) * angle);
+  graphics_context_set_fill_color(ctx, colour);
+  gpath_draw_filled(ctx, path);
+}
+
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  if (next_tide > 0)
+    next_tide--;
+  if (next_tide == 0)
+    request_tide();    
+//  layer_mark_dirty(s_minute_display_layer);
+//  layer_mark_dirty(s_hour_display_layer);
+  layer_mark_dirty(s_segment_layer);
+}
+
+
+
+static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
+  static int showingLevels = -1;
+  if (levelsAlways)
+      tick_timer_service_subscribe(MINUTE_UNIT, handle_second_tick);
+  if (showLevels && showingLevels == -1) {
+    showingLevels = 5;
+  }
+  else if (showLevels && showingLevels > 0) {
+    showingLevels--;
+  }
+  else if (showLevels && showingLevels == 0) {
+    showLevels = false;
+    showingLevels = -1;
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_second_tick);
+    layer_mark_dirty(s_segment_layer);
+  }
+  if ((units_changed & MINUTE_UNIT) != 0)
+    handle_minute_tick(tick_time, units_changed);
+  
+}
+
+
+
+
+static void segment_update_proc(Layer *layer, GContext* ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GPoint center = grect_center_point(&bounds);
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+
+  int hour = t->tm_hour % 12;
+  int minute = t->tm_min;
+
+  bool fillb = true;
+  for (unsigned int i = 15; i < 375; i += 30) {
+    fillb = !fillb;
+    #ifdef PBL_COLOR
+    paint(ctx, s_hour_segment_path, i, fillb ? GColorBlue : GColorBabyBlueEyes);
+    #else
+    paint(ctx, s_hour_segment_path, i, fillb ? GColorBlack : GColorWhite);
+    #endif
+    if (i == 15 || i == 195) {
+      for (int j = 4; j < 15; j+= 7) {
+        fillb = !fillb;
+        #ifdef PBL_COLOR
+        paint(ctx, s_minute_segment_path, j + (i - 15), !fillb ? GColorBlue : GColorBabyBlueEyes);
+        #else
+        paint(ctx, s_minute_segment_path, j + (i - 15), !fillb ? GColorBlack : GColorWhite);
+        #endif
+      }
+    }
+  }
+
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  #ifdef PBL_COLOR
+  graphics_context_set_stroke_width(ctx, 4);
+  graphics_draw_circle(ctx, center, 71);
+  #else
+  graphics_draw_circle(ctx, center, 72);
+  graphics_draw_circle(ctx, center, 71);
+  graphics_draw_circle(ctx, center, 70);
+  
+  
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_circle(ctx, center, 69);
+  graphics_draw_circle(ctx, center, 68);
+  #endif
+    
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_circle(ctx, center, 59);
+  #ifndef PBL_COLOR
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_circle(ctx, center, 59);
+  graphics_draw_circle(ctx, center, 60);
+  #endif
+    
+  graphics_context_set_text_color(ctx, GColorWhite);
+  
+  // Draw the hours to high/low tide
+  for (int i=0; i<5; i++) {
+    graphics_draw_text(ctx, tide_labels[i], fonts_get_system_font(FONT_KEY_GOTHIC_18), tide_pos1[i], GTextOverflowModeFill, GTextAlignmentCenter, NULL);  
+    graphics_draw_text(ctx, tide_labels[i], fonts_get_system_font(FONT_KEY_GOTHIC_18), tide_pos2[i], GTextOverflowModeFill, GTextAlignmentCenter, NULL);  
+  }
+  
+  // Draw various text items
+  graphics_draw_text(ctx, "High Tide", fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(45,0,60,14), GTextOverflowModeFill, GTextAlignmentCenter, NULL);  
+  graphics_draw_text(ctx, "Low Tide", fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(45,150,60,14), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+  
+  #ifdef PBL_COLOR
+   graphics_context_set_text_color(ctx, GColorRajah);
+  #endif
+    if (showLevels || levelsAlways) {
+    
+    if (highlow[0] == 'H') {
+      graphics_draw_text(ctx, "Rising", fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(33,100,80,18), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+      graphics_draw_text(ctx, nlvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0,0,50,18), GTextOverflowModeFill, GTextAlignmentLeft, NULL);  
+      graphics_draw_text(ctx, nlvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(94,0,50,18), GTextOverflowModeFill, GTextAlignmentRight, NULL);
+      graphics_draw_text(ctx, llvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0,140,50,18), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+      graphics_draw_text(ctx, flvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(94,140,50,18), GTextOverflowModeFill, GTextAlignmentRight, NULL);  
+    } else {
+      graphics_draw_text(ctx, "Falling", fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(33,100,80,18), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+      graphics_draw_text(ctx, flvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0,0,50,18), GTextOverflowModeFill, GTextAlignmentLeft, NULL);  
+      graphics_draw_text(ctx, llvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(94,0,50,18), GTextOverflowModeFill, GTextAlignmentRight, NULL);
+      graphics_draw_text(ctx, nlvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(0,140,50,18), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+      graphics_draw_text(ctx, nlvl, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(94,140,50,18), GTextOverflowModeFill, GTextAlignmentRight, NULL);  
+    }
+  }
+      
+    
+  // Draw the tide hand
+  #ifdef PBL_COLOR
+  graphics_context_set_stroke_color(ctx, GColorRed);
+  graphics_context_set_stroke_width(ctx, 5);
+  #else
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  #endif
+  
+int nt = next_tide > 360 ? 360 : next_tide;
+
+  if (highlow[0] == 'L')
+    nt = nt + 360;
+
+    GPoint tidepc_hand = (GPoint) {
+    .x = (int16_t)(-sin_lookup(TRIG_MAX_ANGLE * nt / 720) * (int32_t)(60 * pc / 100.0) / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * nt / 720) * (int32_t)(60 * pc / 100.0) / TRIG_MAX_RATIO) + center.y,
+  };
+
+   
+  GPoint tide_hand = (GPoint) {
+    .x = (int16_t)(-sin_lookup(TRIG_MAX_ANGLE * nt / 720) * (int32_t)(60) / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * nt / 720) * (int32_t)(60) / TRIG_MAX_RATIO) + center.y,
+  };
+#ifdef PBL_COLOR 
+  graphics_draw_line(ctx, center, tide_hand); 
+#else
+  for (int x=0; x<4; x++)
+    for (int y=0; y<4; y++)
+        graphics_draw_line(ctx, GPoint(center.x+x-2, center.y+y-2), tide_hand);
+
+  /*
+  APP_LOG(APP_LOG_LEVEL_INFO, "nt: %d", nt);
+  gpath_rotate_to(s_hour_hand_path, TRIG_MAX_ANGLE * ((720-nt)/720.0));
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  gpath_draw_filled(ctx, s_hour_hand_path);
+  */
+#endif
+  // Circle in the middle (Red on Time, White on BW)
+  graphics_draw_circle(ctx, center, 4);
+
+  
+  
+  #ifdef PBL_COLOR
+  graphics_context_set_stroke_color(ctx, GColorChromeYellow);
+  graphics_context_set_stroke_width(ctx, 2);
+  graphics_draw_circle(ctx, tidepc_hand, 1);
+  #endif
+
+//  graphics_draw_line(ctx, center, tidepc_hand);
+  
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  #ifdef PBL_COLOR
+  graphics_context_set_stroke_width(ctx, 2);
+  #endif
+
+  // Draw the hour hand & tail
+  GPoint hour_hand = (GPoint) {
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * (hour / 12.0 + minute / 720.0)) * (int32_t)(30) / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * (hour / 12.0 + minute / 720.0)) * (int32_t)(30) / TRIG_MAX_RATIO) + center.y,
+  };
+  // APP_LOG(APP_LOG_LEVEL_INFO, "x: %d y: %d next_tide: %d", (int) tide_hand.x, (int) tide_hand.y, next_tide);
+  #ifdef PBL_COLOR
+        graphics_draw_line(ctx, center, hour_hand);
+  #else  
+  for (int x=0; x<2; x++)
+    for (int y=0; y<2; y++)
+        graphics_draw_line(ctx, GPoint(center.x+x-1, center.y+y-1), GPoint(hour_hand.x+x-1,hour_hand.y+y-1));
+  #endif
+    
+  hour_hand = (GPoint) {
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * (((hour+6)%12) / 12.0 + minute / 720.0)) * (int32_t)(10) / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * (((hour+6)%12) / 12.0 + minute / 720.0)) * (int32_t)(10) / TRIG_MAX_RATIO) + center.y,
+  };
+  // APP_LOG(APP_LOG_LEVEL_INFO, "x: %d y: %d next_tide: %d", (int) tide_hand.x, (int) tide_hand.y, next_tide);
+  #ifdef PBL_COLOR
+  graphics_draw_line(ctx, center, hour_hand);
+  #else
+  for (int x=0; x<2; x++)
+    for (int y=0; y<2; y++)
+        graphics_draw_line(ctx, GPoint(center.x+x-1, center.y+y-1), GPoint(hour_hand.x+x-1,hour_hand.y+y-1));
+  #endif  
+  
+  // Draw the minute hand
+  GPoint minute_hand = (GPoint) {
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * minute / 60) * (int32_t)(40) / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * minute / 60) * (int32_t)(40) / TRIG_MAX_RATIO) + center.y,
+  };
+  //APP_LOG(APP_LOG_LEVEL_INFO, "x: %d y: %d next_tide: %d", (int) tide_hand.x, (int) tide_hand.y, next_tide);
+  #ifdef PBL_COLOR
+  graphics_draw_line(ctx, center, minute_hand);
+  #else
+  for (int x=0; x<2; x++)
+    for (int y=0; y<2; y++)
+        graphics_draw_line(ctx, GPoint(center.x+x-1, center.y+y-1), GPoint(minute_hand.x+x-1,minute_hand.y+y-1));
+  #endif    
+  
+  minute_hand = (GPoint) {
+    .x = (int16_t)(sin_lookup(TRIG_MAX_ANGLE * ((minute+30)%60) / 60) * (int32_t)(10) / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(TRIG_MAX_ANGLE * ((minute+30)%60) / 60) * (int32_t)(10) / TRIG_MAX_RATIO) + center.y,
+  };
+  //APP_LOG(APP_LOG_LEVEL_INFO, "x: %d y: %d next_tide: %d", (int) tide_hand.x, (int) tide_hand.y, next_tide);
+  
+  
+  #ifdef PBL_COLOR
+  graphics_draw_line(ctx, center, minute_hand);
+  #else
+  for (int x=0; x<2; x++)
+    for (int y=0; y<2; y++)
+        graphics_draw_line(ctx, GPoint(center.x+x-1, center.y+y-1), GPoint(minute_hand.x+x-1,minute_hand.y+y-1));
+  #endif    
+  
+  // White circle in the middle
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, center, 4); // White circle in the middle
+  // Pin in the middle!
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_pixel(ctx, center);
+}
+
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+  showLevels = true;
+  tick_timer_service_subscribe(SECOND_UNIT | MINUTE_UNIT, handle_second_tick);
+  layer_mark_dirty(s_segment_layer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+//APP_LOG(APP_LOG_LEVEL_INFO, "Message");
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+          DictionaryIterator *iter;
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_NEXT_TIDE:
+      next_tide = (int)t->value->int32;
+      APP_LOG(APP_LOG_LEVEL_INFO, "NextTide: %d", (int)t->value->int32);
+      break;
+    case KEY_HIGHLOW:
+      snprintf(highlow, sizeof(highlow), t->value->cstring);
+      APP_LOG(APP_LOG_LEVEL_INFO, "HighLow: %s", (char *)t->value->cstring);
+      break;
+    case KEY_PC:
+      pc = (int)t->value->int32;
+      APP_LOG(APP_LOG_LEVEL_INFO, "PC: %d", (int)t->value->int32);
+      break;
+    case KEY_READY:
+      break;
+
+      case KEY_FLW_LEVEL:
+      snprintf(flvl, sizeof(flvl), "%s", t->value->cstring);
+      APP_LOG(APP_LOG_LEVEL_INFO, "FollowingLevel: %s", (char *)t->value->cstring);
+      break;
+      
+    case KEY_LAST_LEVEL:
+      snprintf(llvl, sizeof(llvl), "%s", t->value->cstring);
+
+      // APP_LOG(APP_LOG_LEVEL_INFO, "LastLevel: %s", (char *)t->value->cstring);
+      break;
+
+    case KEY_NEXT_LEVEL:
+      snprintf(nlvl, sizeof(nlvl), "%s", t->value->cstring);
+
+     // APP_LOG(APP_LOG_LEVEL_INFO, "NextLevel: %s", (char *)t->value->cstring);
+      break;
+    
+    case KEY_CONFIG_LEVELS:
+      levelsAlways = ((int)t->value->int32 == 1);
+     APP_LOG(APP_LOG_LEVEL_INFO, "Levelsalways: %d", (int)t->value->int32);
+      if (!levelsAlways)
+            tick_timer_service_subscribe(SECOND_UNIT | MINUTE_UNIT, handle_second_tick);
+      break;
+
+      default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+  showLevels = true;
+  layer_mark_dirty(s_segment_layer);
+  
+}
+
+
+void request_tide() {
+      DictionaryIterator *iter;
+      app_message_outbox_begin(&iter);
+      dict_write_uint32(iter, KEY_GETTIDE, 0);
+      app_message_outbox_send();
+}
+
+
+
+static void main_window_load(Window *window) {
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_minute_display_layer = layer_create(bounds);
+  // layer_set_update_proc(s_minute_display_layer, minute_display_update_proc);
+  layer_add_child(window_layer, s_minute_display_layer);
+
+  s_minute_segment_path = gpath_create(&MINUTE_SEGMENT_PATH_POINTS);
+  gpath_move_to(s_minute_segment_path, grect_center_point(&bounds));
+
+  #ifndef PBL_COLOR
+  s_hour_hand_path = gpath_create(&HOUR_HAND_PATH_POINTS);
+  gpath_move_to(s_hour_hand_path, grect_center_point(&bounds));
+  #endif
+  
+  s_hour_display_layer = layer_create(bounds);
+  // layer_set_update_proc(s_hour_display_layer, hour_display_update_proc);
+  layer_add_child(window_layer, s_hour_display_layer);
+
+  s_hour_segment_path = gpath_create(&HOUR_SEGMENT_PATH_POINTS);
+  gpath_move_to(s_hour_segment_path, grect_center_point(&bounds));
+  
+  // Set up the hour segments to high & low tide
+  s_segment_layer = layer_create(bounds);
+  layer_set_update_proc(s_segment_layer, segment_update_proc);
+  layer_add_child(window_layer, s_segment_layer);
+  
+  accel_tap_service_subscribe(tap_handler);
+}
+
+static void main_window_unload(Window *window) {
+  gpath_destroy(s_minute_segment_path);
+  gpath_destroy(s_hour_segment_path);
+
+  layer_destroy(s_minute_display_layer);
+  layer_destroy(s_hour_display_layer);
+}
+
+static void init() {
+
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_open(200, 200);
+  // Open AppMessage
+
+    
+    s_main_window = window_create();
+
+    window_set_background_color(s_main_window, GColorBlack);
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+#ifndef PBL_COLOR
+      window_set_fullscreen(s_main_window, true);
+#endif
+
+  window_stack_push(s_main_window, true);
+
+  // tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  
+    tick_timer_service_subscribe(SECOND_UNIT | MINUTE_UNIT, handle_second_tick);
+
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+
+  tick_timer_service_unsubscribe();
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
